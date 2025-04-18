7e11759a8551e8ec5bdc205ffb627c0aa75bddca
BindonWrathbite <zacherym.thompson@gmail.com>
2025-04-18 08:40:11 -0400

    Completed DTO, added InstrumentMapper class

--- backend/src/main/java/com/ims/backend/dto/InstrumentDTO.java
+++ backend/src/main/java/com/ims/backend/dto/InstrumentDTO.java
@@ -1,4 +1,29 @@
 package com.ims.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class InstrumentDTO {
+
+    private Long id;
+    private String type;
+    private String brand;
+    private String serialNumber;
+    private String inventoryNumber;
+    private String repairs;
+    private String condition;
+    private LocalDate purchaseDate;
+    private BigDecimal purchasePrice;
+    private String notes;
+
+    private Long locationId;
+    private String locationName; // for display purposes
+
+    private Long assignedStudentId;
+    private String assignedStudentName; // for display purposes
 }
